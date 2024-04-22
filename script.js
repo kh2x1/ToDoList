6e1a503b4233f00bbd7baf15c99199a4175e163e
Khallad-خلاد <100222507+kh2x1@users.noreply.github.com>
2024-04-22 10:48:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -116,3 +116,14 @@ function updateLocalStorage() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+
+
+function myFunction() {
+        let element = document.body;
+        element.classList.toggle("dark-mode")
+    }
+
+    function myFun() {
+        let element = document.body;
+        element.classList.toggle("light-mode")
+    }
